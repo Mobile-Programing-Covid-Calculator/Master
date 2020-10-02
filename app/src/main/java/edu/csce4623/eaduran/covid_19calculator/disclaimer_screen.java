2d99d09819ab54f8d0cc5007930edf2d8c0f050b
Estebanadm <70405222+Estebanadm@users.noreply.github.com>
2020-10-02 11:51:47 -0500

    Created Disclaimer Screen
    Set the disclaimer screen as the main one and from there after the ok button is pressed it will lauch the welcome screen. Also got rid of the action bar

---
+++ app/src/main/java/edu/csce4623/eaduran/covid_19calculator/disclaimer_screen.java
@@ -0,0 +1,29 @@
+package edu.csce4623.eaduran.covid_19calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class disclaimer_screen extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disclaimer_screen);
+        button = findViewById(R.id.okButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWelcomeScreen();
+            }
+        });
+    }
+    public void openWelcomeScreen(){
+        Intent intent = new Intent(this,welcome_screen.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/edu/csce4623/eaduran/covid_19calculator/welcome_screen.java
+++ app/src/main/java/edu/csce4623/eaduran/covid_19calculator/welcome_screen.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class welcome_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome_screen);
     }
 }
