59964b56cee5d1f81e9982c7e90ab7a4936bc56d
AveryDeSean <62083127+AveryDeSean@users.noreply.github.com>
2020-10-09 12:18:51 -0500

    Added location screen and navigation to it

---
+++ app/src/main/java/edu/csce4623/eaduran/covid_19calculator/location_screen.java
@@ -0,0 +1,26 @@
+package edu.csce4623.eaduran.covid_19calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class location_screen extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_screen);
+        Button backButton = (Button) findViewById(R.id.backButton);
+       // Spinner stateSpinner = (Spinner) findViewById(R.id.state_spinner);
+
+       // ArrayAdapter<String> stateAdapter = new ArrayAdapter<>(location_screen.this,
+          //      android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.states));
+       // stateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       // stateSpinner.setAdapter(stateAdapter);
+    }
+}
--- app/src/main/java/edu/csce4623/eaduran/covid_19calculator/welcome_screen.java
+++ app/src/main/java/edu/csce4623/eaduran/covid_19calculator/welcome_screen.java
@@ -2,13 +2,28 @@ package edu.csce4623.eaduran.covid_19calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class welcome_screen extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        button = findViewById(R.id.preSelectedButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLocationScreen();
+            }
+        });
+    }
+    public void openLocationScreen(){
+        Intent intent = new Intent(this,location_screen.class);
+        startActivity(intent);
     }
 }
