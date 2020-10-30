80c7adb4cf571ee349a40042f13bc6fe00f4cfc5
AveryDeSean <62083127+AveryDeSean@users.noreply.github.com>
2020-10-30 14:07:57 -0500

    added risk screen

--- app/src/main/java/edu/csce4623/eaduran/covid_19calculator/risk_screen.java
+++ app/src/main/java/edu/csce4623/eaduran/covid_19calculator/risk_screen.java
@@ -3,7 +3,9 @@ package edu.csce4623.eaduran.covid_19calculator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,8 +16,18 @@ public class risk_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_risk_screen);
+        //Globals
         Button back2 = findViewById(R.id.backButton2);
         Button submit = findViewById(R.id.location_submit);
+        Spinner spinnerRisk = (Spinner) findViewById(R.id.spinnerRiskProfile);
+
+        //Set
+
+        ArrayAdapter<String> riskAdapter = new ArrayAdapter<>(risk_screen.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.riskProfiles));
+        riskAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       spinnerRisk.setAdapter(riskAdapter);
+
         back2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
