3d805d50c4f559fcd2a1b58df4f4f0272a484a48
gwromano <70405281+gwromano@users.noreply.github.com>
2020-12-08 18:08:39 -0600

    Create covidInfoAPI.java

---
+++ app/src/main/java/edu/csce4623/eaduran/covid_19calculator/covidInfoAPI.java
@@ -0,0 +1,13 @@
+package edu.csce4623.eaduran.covid_19calculator;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface covidInfoAPI {
+
+    @GET("/v2/counties.json?apiKey=d6f5ac0f7c0143b683aa50bf7bf163dd")
+    Call<List<covidInfo>> loadInfo();
+}
